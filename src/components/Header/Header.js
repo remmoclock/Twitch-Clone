2676f352139ea08cbb63dc5a723f9297e5645d4a
Remmo <remmo@hotmail.fr>
2020-09-16 23:58:54 +0200

    HeaderCss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react"
 import logo from "./assets/IconeTwitch.svg"
 import search from "./assets/Search.svg"
 import menuIco from "./assets/MenuIco.svg"
+import "./HeaderStyle.css"
 
 const Header = () => {
   return (
