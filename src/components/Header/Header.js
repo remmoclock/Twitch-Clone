debb94c44679146d68a07a9ee97efc6315d6f480
Remmo <remmo@hotmail.fr>
2020-09-20 16:40:37 +0200

    NavLink

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import logo from "./assets/IconeTwitch.svg"
 import search from "./assets/Search.svg"
 import menuIco from "./assets/MenuIco.svg"
 import "./HeaderStyle.css"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
@@ -10,10 +11,16 @@ const Header = () => {
       <nav className="headerTop">
         <ul className="listeMenu">
           <li className="liensNav">
-            <img src={logo} alt="logo twitch" className="logo" />
+            <Link className="lien" to="/">
+              <img src={logo} alt="logo twitch" className="logo" />
+            </Link>
           </li>
+          <Link className="lien" to="/">
           <li className="liensNav">Top Games</li>
+          </Link>
+          <Link className="lien" to="/top-streams">
           <li className="liensNav">Top Streams</li>
+          </Link>
           <li className="liensNav">
             <form className="formSubmit">
               <input type="text" className="inputRecherche" />
