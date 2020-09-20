26017c7322cb6684881ea3723714e138d44956c2
Remmo <remmo@hotmail.fr>
2020-09-21 00:51:36 +0200

    LiveCss

--- src/components/Live/Live.js
+++ src/components/Live/Live.js
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react"
 import ReactTwitchEmbedVideo from "react-twitch-embed-video"
 import { useParams } from "react-router-dom"
 import api from "../api"
+import "./LiveStyle.css"
 
 const Live = () => {
   let { slug } = useParams()
-  console.log(slug);
+  // console.log(slug);
 
   const [infoStream, setInfoStream] = useState([])
   const [infoGame, setInfoGame] = useState([])
@@ -16,7 +17,7 @@ const Live = () => {
       const result = await api.get(
         `https://api.twitch.tv/helix/streams?user_login=${slug}`
       )
-      console.log(result);
+      // console.log(result);
 
       let gameID = result.data.data.map((gameid) => {
         return gameid.game_id
@@ -25,7 +26,7 @@ const Live = () => {
       const resultNomGame = await api.get(
         `https://api.twitch.tv/helix/games?id=${gameID}`
       )
-      console.log(resultNomGame);
+      // console.log(resultNomGame);
 
       let nomJeu = resultNomGame.data.data.map((gameName) => {
         return gameName.name
@@ -44,9 +45,9 @@ const Live = () => {
       <ReactTwitchEmbedVideo height="754" width="100%" channel={slug} />
       <div className="contInfo">
         <div className="titreStream">{infoStream.title}</div>
-        <div className="viewers">Viewers : {infoStream.viewer_count} </div>
+        <div className="viewer">Viewers: {infoStream.viewer_count} </div>
         <div className="infogame">
-          Streamer : {infoStream.user_name}, &nbsp; Langue :{" "}
+          Streamer: {infoStream.user_name}, &nbsp; Langue:{" "}
           {infoStream.language}
         </div>
         <div className="nomJeu">Jeu : {infoGame} </div>
