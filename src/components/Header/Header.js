bd26132f6338aba08463b845faf3221ec6c64017
Remmo <remmo@hotmail.fr>
2020-09-21 17:54:31 +0200

    navCondition

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,102 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import logo from "./assets/IconeTwitch.svg"
 import search from "./assets/Search.svg"
 import menuIco from "./assets/MenuIco.svg"
+import croix from "./assets/Croix.svg"
 import "./HeaderStyle.css"
 import { Link } from "react-router-dom"
 
 const Header = () => {
+  const [menu, showMenu] = useState(false)
+  const [smallScreen, setSmallScreen] = useState(false)
+  const [searchInput, setSearch] = useState("")
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 900px)")
+    mediaQuery.addListener(handleMediaQueryChange)
+    handleMediaQueryChange(mediaQuery)
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange)
+    }
+  })
+
+  const handleMediaQueryChange = (mediaQuery) => {
+    if (mediaQuery.matches) {
+      setSmallScreen(true)
+    } else {
+      setSmallScreen(false)
+    }
+  }
+
+  const toggleNavRes = () => {
+    showMenu(!menu)
+  }
+
+  const hideMenu = () => {
+    if (menu) {
+      showMenu(!menu)
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const handleKeyPress = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
     <div>
       <nav className="headerTop">
-        <ul className="listeMenu">
-          <li className="liensNav">
+        {(menu || !smallScreen) && (
+          <ul className="listeMenu">
+            <li onClick={hideMenu} className="liensNav">
+              <Link className="lien" to="/">
+                <img src={logo} alt="logo twitch" className="logo" />
+              </Link>
+            </li>
             <Link className="lien" to="/">
-              <img src={logo} alt="logo twitch" className="logo" />
+              <li onClick={hideMenu} className="liensNav">
+                Top Games
+              </li>
             </Link>
-          </li>
-          <Link className="lien" to="/">
-          <li className="liensNav">Top Games</li>
-          </Link>
-          <Link className="lien" to="/top-streams">
-          <li className="liensNav">Top Streams</li>
-          </Link>
-          <li className="liensNav">
-            <form className="formSubmit">
-              <input type="text" className="inputRecherche" />
-              <button type="submit">
-                <img src={search} alt="icone loupe" className="logoLoupe" />
-              </button>
-            </form>
-          </li>
-        </ul>
+            <Link className="lien" to="/top-streams">
+              <li onClick={hideMenu} className="liensNav">
+                Top Streams
+              </li>
+            </Link>
+            <li className="liensNav">
+              <form className="formSubmit" onSubmit={handleSubmit}>
+                <input
+                  required
+                  value={searchInput}
+                  onChange={(e) => handleKeyPress(e)}
+                  type="text"
+                  className="inputRecherche"
+                />
+                <Link
+                  className="lien"
+                  to={{
+                    pathname: `/resultats/${searchInput}`,
+                  }}
+                >
+                  <button type="submit">
+                    <img src={search} alt="icone loupe" className="logoLoupe" />
+                  </button>
+                </Link>
+              </form>
+            </li>
+          </ul>
+        )}
       </nav>
       <div className="menuResBtn">
-        <img src={menuIco} alt="icone menu responsive" className="menuIco" />
+        <img
+          onClick={toggleNavRes}
+          src={!menu ? menuIco : croix}
+          alt="icone menu responsive"
+          className="menuIco"
+        />
       </div>
     </div>
   )
