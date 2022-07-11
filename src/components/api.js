4b5ba5ee3bf9fa84e72132197d95aee2168783de
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-07-11 17:29:29 +0200

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -2,15 +2,18 @@ import axios from "axios"
 
 let api = axios.create({
   headers: {
-    "Client-ID": "yjflf1qawjyhb8llxq1u3bkc9bk4lv",
-    Authorization: "Bearer d0n2v5v239kh1y12xbjgaxw68w3t1g", // netlify
-    // Authorization: "Bearer z4ci2m5dedmn3f7x06sz99aa291vt1", // Localhost3000
+    "Client-ID": "o6qbhh9eliz9tmeqf1ibdqfmgpss86",
+    Authorization: "Bearer rk5pado0fi4a1rcp9wr9sxtvuodmma", // netlify
   },
 })
 
-// CLIENT_ID = yjflf1qawjyhb8llxq1u3bkc9bk4lv
-// REDIRECT = "http://localhost:3000/"
 
-//  LIEN AUTH = https://id.twitch.tv/oauth2/authorize?client_id=yjflf1qawjyhb8llxq1u3bkc9bk4lv&redirect_uri=https://remmo-twitch.netlify.app/&response_type=token
+// REDIRECT = "http://localhost:3000/"
+// REDIRECT = "https://127.0.0.1:3000/
+// REDIRECT = "https://remmo-twitch.netlify.app/"
+ 
+
+//  LIEN AUTH = https://id.twitch.tv/oauth2/authorize?client_id=o6qbhh9eliz9tmeqf1ibdqfmgpss86&redirect_uri=https://remmo-twitch.netlify.app/&response_type=token
+//  LIEN AUTH = https://id.twitch.tv/oauth2/authorize?client_id=o6qbhh9eliz9tmeqf1ibdqfmgpss86&redirect_uri=http://127.0.0.1/&response_type=token
 
 export default api
